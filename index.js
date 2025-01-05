76ef9306c7bbf22ce1f01bb5728c5d5c6e86021d
yogeshgheu <yogeshgheu@gmail.com>
2025-01-05 19:55:24 +0530

    add github api

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
-import 'dotenv/config'
+import "dotenv/config";
 
 const app = express();
+
+const URL = "https://api.github.com/users/yogeshgheu";
 
 app.get("/", (req, res) => {
 	res.send("hello world");
@@ -13,6 +15,12 @@ app.get("/twitter", (req, res) => {
 
 app.get("/login", (req, res) => {
 	res.send("<h1>My name is yogesh</h1>");
+});
+
+app.get("/github", async (req, res) => {
+	const req = await fetch(URL);
+	const response = await res.json();
+	res.json(response);
 });
 
 app.listen(process.env.PORT, () => {
